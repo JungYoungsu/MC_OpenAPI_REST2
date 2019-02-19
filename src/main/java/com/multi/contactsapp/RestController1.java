8aee1180964bef0363f4eb5575f6c729d2fd4c98
JungYoungsu <terun707@naver.com>
2019-02-19 14:03:48 +0900

    Remove ContentNegotiatingViewResolver & Apply MessageConverter

--- src/main/java/com/multi/contactsapp/RestController1.java
+++ src/main/java/com/multi/contactsapp/RestController1.java
@@ -1,29 +1,23 @@
 package com.multi.contactsapp;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.multi.contactsapp.domain.ContactList;
 import com.multi.contactsapp.service.ContactService;
 
-@Controller
-@RequestMapping(value="/contacts")
+@RestController
+@RequestMapping(value = "/contacts")
 public class RestController1 {
-	
+
 	@Autowired
 	private ContactService contactService;
-
-	// http://localhost:8000/MC_OpenAPI_REST2/contacts
-	// http://localhost:8000/MC_OpenAPI_REST2/contacts?output=json
-	// http://localhost:8000/MC_OpenAPI_REST2/contacts?output=xml
-	@RequestMapping(method=RequestMethod.GET)
-	public ModelAndView getContactList() {
-		ModelAndView mav = new ModelAndView();
-		mav.addObject("data", contactService.getContactList());
-		mav.setViewName("contact");
-		return mav;
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public ContactList getContactList() {
+		return contactService.getContactList();
 	}
 
 }
