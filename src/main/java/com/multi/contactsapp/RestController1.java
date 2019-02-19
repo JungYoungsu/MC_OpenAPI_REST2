6cea78c3d8ce57eab944bad80900a8e10fc6d055
JungYoungsu <terun707@naver.com>
2019-02-19 16:45:25 +0900

    add PUT, DELETE, POST

--- src/main/java/com/multi/contactsapp/RestController1.java
+++ src/main/java/com/multi/contactsapp/RestController1.java
@@ -1,11 +1,14 @@
 package com.multi.contactsapp;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.multi.contactsapp.domain.Contact;
 import com.multi.contactsapp.domain.ContactList;
+import com.multi.contactsapp.domain.Result;
 import com.multi.contactsapp.service.ContactService;
 
 @RestController
@@ -22,4 +25,18 @@ public class RestController1 {
 		return contactService.getContactList();
 	}
 
+	@RequestMapping(method = RequestMethod.POST)
+	public Result insertContact(@RequestBody Contact c) {
+		return contactService.insertContact(c);
+	}
+
+	@RequestMapping(method = RequestMethod.DELETE)
+	public Result deleteContact(@RequestBody Contact c) {
+		return contactService.deleteContact(c);
+	}
+
+	@RequestMapping(method = RequestMethod.PUT)
+	public Result updateContact(@RequestBody Contact c) {
+		return contactService.updateContact(c);
+	}
 }
